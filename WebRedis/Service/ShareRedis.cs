2095b0a588a5fd665c551a27b521285dcb9b4694
Liu Link <ctliu5link@hotmail.com>
2025-04-25 10:10:24 +0800

    添加RedisDatabase enum

--- WebRedis/Service/ShareRedis.cs
+++ WebRedis/Service/ShareRedis.cs
@@ -5,23 +5,39 @@ namespace WebRedis.Service
 {
     public class ShareRedis(IRedisCache redisCache)
     {
+        /// <summary>
+        /// RedisDB enum for database selection.
+        /// </summary>
+        enum RedisDatabase
+        {
+            Default = 0,
+            JSON_Data = 1,
+            PdfViewer = 2,
+        }
+        private void changeBD(RedisDatabase redisDB)
+        {
+            redisCache.SelectDatabase((int)redisDB);
+        }
+
+        #region Database(1)
         const string keyForCachedFile = "CachedFile"; //請自行確保這個key不會重複而碰撞
         public TemplateCachedFile? CachedFile
         {
             get
             {
-                redisCache.SelectDatabase(1);
+                changeBD(RedisDatabase.JSON_Data);
                 return redisCache.StringGet<TemplateCachedFile>(keyForCachedFile);
             }
             set
             {
-                redisCache.SelectDatabase(1);
+                changeBD(RedisDatabase.JSON_Data);
                 if (value is not null)
                     redisCache.StringSet(keyForCachedFile, value); // 敏感內容，請自行加密
                 else
                     redisCache.StringDelete<TemplateCachedFile>(keyForCachedFile);
             }
         }
+        #endregion
 
     }
 }
